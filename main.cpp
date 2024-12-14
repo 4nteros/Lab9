a56d6ec43c69257507d84c29ddca965168106ffc
4nteros <bogatovandrej345@gmail.com>
2024-12-14 08:06:38 +0200

    first commit

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+int main() 
+{
+    int num;
+    std::cout << "Enter number: ";
+    std::cin >> num;
+
+    if (num > INT_MAX || num < INT_MIN)
+    {
+        return -1;
+    }
+
+    std::cout << &num << std::endl;
+
+    unsigned char* bytePtr = reinterpret_cast<unsigned char*>(&num);  
+
+    std::cout << "Contents of each byte of a number " << num << std::endl;
+    for (size_t i = 0; i < sizeof(num); i++) 
+    {
+        std::cout << "Byte " << i << ": " << static_cast<int>(bytePtr[i]) << std::endl; 
+    }
+
+    return 0;
+}
